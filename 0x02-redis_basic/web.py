7e273f19f03bd70b53d745e48e4f4828557923b7
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2023-05-08 05:09:51 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Redis Module """
-efficient efficient efficient effiriceint efficeint
+efficient efficient efficient effiriceint efficein
 from functools import wraps
 import redis
 import requests
