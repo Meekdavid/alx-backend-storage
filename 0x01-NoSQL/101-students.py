1d1fe98c06513f781f4d75e87975ca875dfcfdb1
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2023-05-14 06:26:00 +0100

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ MongoDB Operations with Python using pymongo """
-efficient effe
+efficient eff
 
 def top_students(mongo_collection):
     # sourcery skip: inline-immediately-returned-variable
